1f891618e2aafc573e74313e499a539dfc4869a7
Яна Котова <63591235+JanaKotova@users.noreply.github.com>
2020-04-16 01:08:11 +0500

    Update

--- Example/bot.py
+++ Example/bot.py
@@ -11,7 +11,7 @@ async def on_message(message):
  if message.author == bot.user:
   return
  if message.content.lower() == ("?help"):
-  await message.channel.send("Список команд:\n1.) ?random\n2.) Команда")
+  await message.channel.send("Список команд:\n1.) ?random\n2.) ?скоро")
  if message.content.lower() == ("?random"):
   await message.channel.send(random.randint(0,10))
 bot.run(TOKEN)
