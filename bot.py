128de4849f83c55d46076a2b0b76e19901f03e1a
Яна Котова <63591235+JanaKotova@users.noreply.github.com>
2020-04-16 00:14:16 +0500

    random add

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 bot = discord.Client()
 
@@ -10,6 +11,7 @@ async def on_message(message):
  if message.author == bot.user:
   return
  if message.content.lower() == ("?help"):
-  await message.channel.send("Список команд:\n1.) Команда\n2.) Команда")
-
+  await message.channel.send("Список команд:\n1.) ?random\n2.) Команда")
+ if message.content.lower() == ("?random"):
+  await message.channel.send(random.randint(0,10))
 bot.run(TOKEN)
